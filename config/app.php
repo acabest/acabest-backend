29a1dbc27c511829b0443f3dfc220dc9be79676c
Ghaffaru <mudashiruagm@gmail.com>
2020-06-19 09:07:59 +0000

    fix app config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Acabest'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://acabest.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
