077f634725b9728562a921adfeecc01addeac4d1
Ghaffaru <mudashiruagm@gmail.com>
2020-09-27 20:40:08 +0000

    aded id

--- app/Http/Resources/SubcategoryResource.php
+++ app/Http/Resources/SubcategoryResource.php
@@ -15,6 +15,7 @@ class SubcategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'subsubcategories' => $this->subsubcategories
         ];
